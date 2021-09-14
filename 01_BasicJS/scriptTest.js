d10deffdf5a794c02e811d5d3182e9685b8b2a72
Thanawat Naeching <thanawat.jack@mail.kmutt.ac.th>
2021-09-14 12:44:54 +0700

    Add File script

--- 01_BasicJS/scriptTest.js
+++ 01_BasicJS/scriptTest.js
@@ -71,62 +71,77 @@ console.log(zodiac(2544)); */
 // console.log(BMI(130.1,66.63543307))
 // console.log(BMI(150,88))
 
-function Payment(totalPrice, custPay){
-    let exchange = custPay - totalPrice;
-    console.log('ราคาสินค้า :'+' '+totalPrice+' '+'บาท');
-    console.log('รับเงินมา :'+' '+ custPay+' '+'บาท');
-    console.log('เงินทอน :'+' '+exchange +' '+'บาท');
-    let bank = {
-        bank_500_BTH : 0, 
-        bank_100_BTH : 0, 
-        bank_50_BTH : 0,  
-        bank_20_BTH : 0, 
-        coin_10_BTH : 0,  
-        coin_5_BTH : 0, 
-        coin_2_BTH : 0, 
-        coin_1_BTH : 0
-    }
+// function Payment(totalPrice, custPay){
+//     let exchange = custPay - totalPrice;
+//     console.log('ราคาสินค้า :'+' '+totalPrice+' '+'บาท');
+//     console.log('รับเงินมา :'+' '+ custPay+' '+'บาท');
+//     console.log('เงินทอน :'+' '+exchange +' '+'บาท');
+//     let bank = {
+//         bank_500_BTH : 0, 
+//         bank_100_BTH : 0, 
+//         bank_50_BTH : 0,  
+//         bank_20_BTH : 0, 
+//         coin_10_BTH : 0,  
+//         coin_5_BTH : 0, 
+//         coin_2_BTH : 0, 
+//         coin_1_BTH : 0
+//     }
 
-    while(exchange > 0){
-        if(exchange >= 500){
-            exchange = exchange-500;
-            bank.bank_500_BTH++;
-        } else if(exchange >= 100){
-            exchange = exchange-100;
-            bank.bank_100_BTH++;
-        } else if(exchange >= 50){
-            exchange = exchange-50;
-            bank.bank_50_BTH++;
-        } else if(exchange >= 20){
-            exchange = exchange-20;
-            bank.bank_20_BTH++;
-        } else if(exchange >= 10){
-            exchange = exchange-10;
-            bank.coin_10_BTH++;
-        } else if(exchange >= 5){
-            exchange = exchange-5;
-            bank.coin_5_BTH++;
-        } else if(exchange >= 2){
-            exchange = exchange-2;
-            bank.coin_2_BTH++;
-        } else {
-            exchange = exchange-1;
-            bank.coin_1_BTH++;
-        }
-    }
+//     while(exchange > 0){
+//         if(exchange >= 500){
+//             exchange = exchange-500;
+//             bank.bank_500_BTH++;
+//         } else if(exchange >= 100){
+//             exchange = exchange-100;
+//             bank.bank_100_BTH++;
+//         } else if(exchange >= 50){
+//             exchange = exchange-50;
+//             bank.bank_50_BTH++;
+//         } else if(exchange >= 20){
+//             exchange = exchange-20;
+//             bank.bank_20_BTH++;
+//         } else if(exchange >= 10){
+//             exchange = exchange-10;
+//             bank.coin_10_BTH++;
+//         } else if(exchange >= 5){
+//             exchange = exchange-5;
+//             bank.coin_5_BTH++;
+//         } else if(exchange >= 2){
+//             exchange = exchange-2;
+//             bank.coin_2_BTH++;
+//         } else {
+//             exchange = exchange-1;
+//             bank.coin_1_BTH++;
+//         }
+//     }
 
-    for(let money in bank){
-        if(bank[money] != 0){
-        console.log(`${money} : ${bank[money]}`)
-        }
-    }
-    //return bank;
+//     for(let money in bank){
+//         if(bank[money] != 0){
+//         console.log(`${money} : ${bank[money]}`)
+//         }
+//     }
+//     //return bank;
+// }
+
+// console.log(Payment(200,1000))
+// console.log('-----------------')
+// console.log(Payment(124,500))
+// console.log('-----------------')
+// console.log(Payment(48,100))
+let per1 = { id: 1, name: 'Joe' };
+
+let per2 = per1;
+
+
+
+doSomething(per2);
+
+function doSomething(p) {
+
+  p.name = 'Mary';
+
 }
 
-console.log(Payment(200,1000))
-console.log('-----------------')
-console.log(Payment(124,500))
-console.log('-----------------')
-console.log(Payment(48,100))
+console.log(per1);
 
 
